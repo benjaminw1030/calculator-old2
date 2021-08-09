c05c6e4d32903efe7e952a66423fd77753834d63
Ben Wilson <benjaminw1030@gmail.com>
2021-08-09 11:52:07 -0700

    add calculator business logic
    Co-authored-by: Brad Prettyman <brad.prettyman5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,22 @@ function add(number1, number2) {
 	return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2));
+alert("The sum is: " + add(number1, number2).toString() + ".");
+alert("The difference is: " + subtract(number1, number2).toString() + ".");
+alert("The product is: " + multiply(number1, number2).toString() + ".");
+alert("The quotient is: " + divide(number1, number2).toString() + ".");
